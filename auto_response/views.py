5d2507f65852a221e748cacece3564cf0d083f33
Amritendu <linux.amrit@gmail.com>
2022-08-25 18:33:50 +0530

    Second commit

--- auto_response/views.py
+++ auto_response/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+@csrf_exempt
+def hello_world(request):
+    if str(request.method).upper() == 'GET':
+        return HttpResponse('<h2>GET Request .... Greetings from India !!!!!!!!</h2>')
+    elif str(request.method).upper() == 'POST':
+        print(request.POST)
+        print(request.POST)
+        print(request.POST.items())
+        data = request.POST.items()
+        print(data)
+        for k in data:
+            print("k", str(k))
+        return HttpResponse('<h2>POST Request Greetings from India !!!!!!!!</h2>')
